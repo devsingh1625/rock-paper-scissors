d1b479266d790e58648135fc2f2a86734479b990
devsingh1625 <devsingh1625@gmail.com>
2024-10-22 17:51:21 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+public class Main {
+    public static void main(String[] args)
+    {
+        String [] rps = {"r","p","s"};// creating a list names rps which includes rock, paper, and scissors
+        String computerMove = rps[new Random().nextInt(rps.length)];
+
+
+
+
+
+    }
+}
